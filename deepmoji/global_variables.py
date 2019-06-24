3a28496e61cd1cd9965eb3f7c3aff6d3e54ef846
Amit <meamitkc@gmail.com>
2019-06-24 19:07:55 +0545

    Make package+data installable via pip

--- deepmoji/global_variables.py
+++ deepmoji/global_variables.py
@@ -15,7 +15,7 @@ SPECIAL_TOKENS = ['CUSTOM_MASK',
                   'CUSTOM_BREAK']
 SPECIAL_TOKENS.extend(['{}BLANK_{}'.format(SPECIAL_PREFIX, i) for i in range(6, 10)])
 
-ROOT_PATH = dirname(dirname(abspath(__file__)))
+ROOT_PATH = dirname(abspath(__file__))
 VOCAB_PATH = '{}/model/vocabulary.json'.format(ROOT_PATH)
 PRETRAINED_PATH = '{}/model/deepmoji_weights.hdf5'.format(ROOT_PATH)
 
