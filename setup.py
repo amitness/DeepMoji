cf44e4323cf6672aa806b860b2ef50914acf45e6
Amit Chaudhary <meamitkc@gmail.com>
2021-01-12 14:55:17 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'emoji==0.4.5',
-        'h5py==2.8.0',
         'numpy',
         'scikit-learn',
         'text-unidecode',
-        'tensorflow==2.1.0',
+        'tensorflow',
     ],
 )
